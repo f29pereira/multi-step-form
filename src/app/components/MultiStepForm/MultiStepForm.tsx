bc34859dba45d1e47e603c7bb0c2d9ff7dd0c76e
Filipe Pereira <f29.pereira@gmail.com>
2026-06-03 21:55:59 +0100

    feat: Add Go Back, Next Step and Confirm buttons to the MultiStepForm component

--- src/app/components/MultiStepForm/MultiStepForm.tsx
+++ src/app/components/MultiStepForm/MultiStepForm.tsx
@@ -3,14 +3,32 @@
 import styles from "./MultiStepForm.module.css";
 import { useState } from "react";
 import StepsList from "./StepsList/StepsList";
+import Button from "../ui/Button/Button";
 
 /**
- * Renders the multi-step user subscription form
+ * Renders the multi-step user subscription form with:
+ * - Steps List
+ * - Current form
+ * - Go Back and Next or Confirm buttons
  */
 export default function MultiStepForm() {
   const [currentStep, setCurrentStep] = useState<number>(0);
 
   const stepsList = ["1", "2", "3", "4"];
+
+  /**
+   * Goes to the next form step
+   */
+  const nextStep = () => {
+    setCurrentStep((prev) => prev + 1);
+  };
+
+  /**
+   * Goes back to the previous form step
+   */
+  const prevStep = () => {
+    setCurrentStep((prev) => prev - 1);
+  };
 
   return (
     <section className={styles.sectionCont}>
@@ -18,9 +36,32 @@ export default function MultiStepForm() {
         <StepsList list={stepsList} currentStepIndex={currentStep} />
       </div>
 
-      {/*TO DO - add current form step*/}
+      <div>
+        {/*TO DO - add current form step*/}
 
-      {/*TO DO - add steps navigation*/}
+        {/*Navigation buttons*/}
+        <div className={styles.navigationCont}>
+          {currentStep > 0 ? (
+            <Button
+              description="Go Back"
+              variant="transparentBtn"
+              handleOnClick={prevStep}
+            />
+          ) : null}
+
+          <div className={styles.nextConfirmBtnCont}>
+            {currentStep === 3 ? (
+              <Button description="Confirm" variant="purpleBtn" />
+            ) : (
+              <Button
+                description="Next Step"
+                variant="blueBtn"
+                handleOnClick={nextStep}
+              />
+            )}
+          </div>
+        </div>
+      </div>
     </section>
   );
 }

d8a63259cdd70dba2ba12b674bc57c883ad9cf58
Filipe Pereira <f29.pereira@gmail.com>
2026-06-09 16:22:12 +0100

    feat: Update Plan component
    - Update Plan component to render a label and radio button instead off a button
    - Add styling for Desktop (1024px) screens
    - Update Plan.util with new function formatPlanPrice
    
    Update index.ts types:
    - PlanDetails: add plan id
    - PlanPricing: update the value property to be a number instead of a string
    - PlanProps: add new properties id, selectedPlan and setCurrentPlan properties

--- src/app/components/MultiStepForm/Forms/SelectPlan/Plan/Plan.tsx
+++ src/app/components/MultiStepForm/Forms/SelectPlan/Plan/Plan.tsx
@@ -1,20 +1,42 @@
+"use client"; // Client Component
 import styles from "./Plan.module.css";
 import type { PlanProps } from "@/app/components/types";
-import { getPlanIcon } from "./Plan.util";
+import { getPlanIcon, formatPlanPrice } from "./Plan.util";
+import { useMultiStepForm } from "@/app/components/customHooks/useMultiStepForm";
 
 /**
  * Renders a subscription plan with:
- * - Icon
+ * - Plan Icon
  * - Plan type
  * - Price (monthly or yearly value)
  * - Discount if applicable
  */
-export default function Plan({ type, price }: PlanProps) {
+export default function Plan({
+  id,
+  type,
+  price,
+  selectedPlan,
+  setCurrentPlan,
+}: PlanProps) {
+  const { isYearly } = useMultiStepForm();
+
   const icon = getPlanIcon(type);
 
   return (
-    <button className={styles.btn}>
-      {/*Icon*/}
+    <label
+      className={`${styles.planCont} ${selectedPlan === id ? styles.selected : ""}`}
+    >
+      <input
+        className="sr-only"
+        type="radio"
+        name="plan"
+        onChange={() => {
+          setCurrentPlan(id);
+        }}
+        checked={selectedPlan === id}
+      />
+
+      {/*Plan Icon*/}
       <div>{icon}</div>
 
       <div className={styles.contentCont}>
@@ -22,13 +44,18 @@ export default function Plan({ type, price }: PlanProps) {
         <span className={`bold-text ${styles.type}`}>{type}</span>
 
         {/*Price (monthly or yearly value)*/}
-        <span className={`light-text ${styles.value}`}>{price.value}</span>
+        <span className="sr-only">{`Price ${price.value} dollars per ${isYearly ? "year" : "month"}`}</span>
+
+        <span className={`light-text ${styles.value}`} aria-hidden="true">
+          {formatPlanPrice(isYearly, price.value)}
+        </span>
 
         {/*Discount*/}
+        <span className="sr-only">Discount</span>
         <span className={`lighter-text ${styles.discount}`}>
           {price.discount}
         </span>
       </div>
-    </button>
+    </label>
   );
 }

fa5721067b064972547d5f45b375c21d7986b55d
Filipe Pereira <f29.pereira@gmail.com>
2026-06-24 11:41:56 +0100

    test: Update unit testing for the SubscriptionToggle component
    - Add test that expects the function toggleSubscription to be called when the subscription button is clicked

--- __tests__/unit/components/MultiStepForm/Forms/SelectPlan/SubscriptionToggle/SubscriptionToggle.test.tsx
+++ __tests__/unit/components/MultiStepForm/Forms/SelectPlan/SubscriptionToggle/SubscriptionToggle.test.tsx
@@ -1,4 +1,5 @@
-import { render } from "@testing-library/react";
+import { screen, render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import SubscriptionToggle from "@/app/components/MultiStepForm/Forms/SelectPlan/SubscriptionToggle/SubscriptionToggle";
 import { createEmptyMultiStepFormContext } from "../../../../../../../fixtures/multiStepForm.fixtures";
 import { expectSubscriptionToggleVisible } from "../../../../../../helpers/multiStepForm.helpers";
@@ -20,5 +21,11 @@ describe("SubscriptionToggle component", () => {
 
   it("renders the monthly and yearly text and the toggle button", () => {
     expectSubscriptionToggleVisible(context.formData.isYearly);
+  });
+
+  it("calls the toggleSubscription function when clicking the toggle button", async () => {
+    const btn = screen.getByRole("button");
+    await userEvent.click(btn);
+    expect(context.toggleSubscription).toHaveBeenCalledTimes(1);
   });
 });

9f124b55771b5b8b542d9feee94fcee1a8ddc651
Filipe Pereira <f29.pereira@gmail.com>
2026-07-05 12:22:25 +0100

    chore: Update Next.js config file
    - Add isPlaywright variable to build Next.js app with no prefix for the Playwright job

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
+const isPlaywright = process.env.PLAYWRIGHT === "true";
+
 const nextConfig: NextConfig = {
   output: "export", // Static export for GitHub Pages
-  basePath: "/multi-step-form", //GitHub Pages subpath
-  assetPrefix: "/multi-step-form/", // Prefix assets with repo path
+  basePath: isPlaywright ? "" : "/multi-step-form", //GitHub Pages subpath
+  assetPrefix: isPlaywright ? "" : "/multi-step-form/", // Prefix assets with repo path
   images: {
     unoptimized: true, // Disable Next.js image optimization
   },

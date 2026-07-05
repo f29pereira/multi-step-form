c1404da048f6aaf65ee9ddf85fa7be8c86f25b9e
Filipe Pereira <f29.pereira@gmail.com>
2026-07-05 12:02:15 +0100

    chore: Configure Next.js for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // Static export for GitHub Pages
+  basePath: "/multi-step-form", //GitHub Pages subpath
+  assetPrefix: "/multi-step-form/", // Prefix assets with repo path
+  images: {
+    unoptimized: true, // Disable Next.js image optimization
+  },
 };
-
-export default nextConfig;

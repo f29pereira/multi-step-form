55e3903569173a38f5dd1b129cdab9bd6789aa95
Filipe Pereira <f29.pereira@gmail.com>
2026-08-05 19:32:52 +0100

    chore: Update Playwright config file
    - Update the webServer command, as GitHub Pages will be replaced by Vercel for hosting the app

--- playwright.config.ts
+++ playwright.config.ts
@@ -113,9 +113,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: process.env.CI
-      ? "npx next build && npx serve@latest out -l 3000"
-      : "npm run build && npm run start",
+    command: "npm run build && npm run start",
     url: "http://localhost:3000",
     reuseExistingServer: !process.env.CI,
     timeout: 120 * 1000,
